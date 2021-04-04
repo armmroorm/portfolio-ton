983af6cf8a064a8ec40c09bac814348edb5a29b0
WBW <arram.s@moomall.com>
2021-04-04 14:21:58 +0700

    success

--- components/BrandCommerce.vue
+++ components/BrandCommerce.vue
@@ -1,13 +1,18 @@
 <template>
   <slide-carousel :settings="settings">
-    <div v-for="item in items" class="text-center justify-center">
-      <v-img
-        :lazy-src="require(`~/assets/img/${item.img}.png`)"
-        max-width="250"
-        max-height="150"
-        class="mb-4"
-        :src="require(`~/assets/img/${item.img}.png`)"
-      ></v-img>
+    <div v-for="item in items" class="text-center px-4">
+      <v-card
+        class="rounded-pill mb-6"
+        outlined
+      >
+        <v-img
+          :lazy-src="require(`~/assets/img/${item.img}.png`)"
+          max-width="250"
+          max-height="150"
+          class="mx-auto "
+          :src="require(`~/assets/img/${item.img}.png`)"
+        ></v-img>
+      </v-card>
 
       <div class="title">{{ item.title }}</div>
       <div class="subtitle">{{ item.subtitle }}</div>
@@ -21,16 +26,45 @@ export default {
   data() {
     return {
       items: [
-        { img: 'KMUTT', title: 'Oppo', subtitle: 'Jr.Designer' }
+        { img: 'thisshop', title: 'This shop', subtitle: 'Jr.Designer' },
+        { img: 'thisshop', title: 'This shop', subtitle: 'Jr.Designer' },
+        { img: 'thisshop', title: 'This shop', subtitle: 'Jr.Designer' },
+        { img: 'thisshop', title: 'This shop', subtitle: 'Jr.Designer' },
+        { img: 'thisshop', title: 'This shop', subtitle: 'Jr.Designer' }
       ],
       settings: {
         'dots': true,
-        'focusOnSelect': true,
         'infinite': true,
         'speed': 500,
-        'slidesToShow': 3,
-        'slidesToScroll': 3,
-        'touchThreshold': 5
+        'slidesToShow': 4,
+        'slidesToScroll': 4,
+        'initialSlide': 0,
+        'responsive': [
+          {
+            'breakpoint': 1024,
+            'settings': {
+              'slidesToShow': 3,
+              'slidesToScroll': 3,
+              'infinite': true,
+              'dots': true
+            }
+          },
+          {
+            'breakpoint': 600,
+            'settings': {
+              'slidesToShow': 2,
+              'slidesToScroll': 2,
+              'initialSlide': 2
+            }
+          },
+          {
+            'breakpoint': 480,
+            'settings': {
+              'slidesToShow': 1,
+              'slidesToScroll': 1
+            }
+          }
+        ]
       }
     }
   }
